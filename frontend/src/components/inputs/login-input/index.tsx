6689c8b80a9d37ac0853aef25c41582cb12eb9ea
Vikas <vk.pratap98@gmail.com>
2022-08-14 23:41:56 +0530

    login component partially completed

---
+++ frontend/src/components/inputs/login-input/index.tsx
@@ -0,0 +1,43 @@
+import { ClassAttributes, FC, InputHTMLAttributes } from "react";
+import { useField, FieldHookConfig, ErrorMessage } from "formik";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>, ClassAttributes<HTMLInputElement> {
+  bottom?: boolean;
+}
+
+type IProps = InputProps &
+  FieldHookConfig<string>;
+
+const LoginInput: FC<IProps> = (props) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <div className="w-80 flex relative flex-col items-center">
+      {meta.touched && meta.error && !props.bottom && (
+        <div className="relative py-3 px-2 bg-[#e84118] w-full text-[white] text-sm rounded mb-3 md:absolute md:w-80 md:-left-[21rem] md:top-[2.2px]">
+          {<ErrorMessage name={field.name} />}
+          <div className="absolute bottom-0 translate-y-full border-[#e84118] border-t-8 border-solid border-r-8 border-r-[transparent] border-l-8 border-l-[transparent] md:border-l-[#e84118] md:border-t-[transparent] md:border-b-8 md:border-b-[transparent] md:border-r-0 md:bottom-1/2 md:right-0 md:translate-x-full md:translate-y-1/2"></div>
+        </div>
+      )}
+      <input
+        {...field}
+        {...props}
+        className={`outline-none border border-solid border-bg__third bg-primary w-full h-12 text-base rounded px-3 mb-3 text-color__primary focus:border-blue__color ${
+          meta.touched && meta.error && "border-[#e84118]"
+        } ${props.className}`}
+      />
+      {meta.touched && meta.error && (
+        <i className={`error_icon absolute right-1 ${props.bottom?"top-[calc(50%-44px)]" :"top-[calc(50%+12px)]"}  scale-[0.8] md:top-1/2 md:-translate-y-[calc(50%+6px)]`}  />
+      )}
+      {meta.touched && meta.error && props.bottom && (
+        <div className="relative py-3 px-2 bg-[#e84118] w-full text-[white] text-sm rounded mb-3 md:absolute md:-left-[21rem] md:top-[2.2px]">
+          {<ErrorMessage name={field.name} />}
+          <div className="absolute top-0 -translate-y-full border-[#e84118] border-b-8 border-solid border-r-8 border-r-[transparent] border-l-8 border-l-[transparent] md:border-l-[#e84118] md:border-t-[transparent] md:border-t-8 md:border-b-[transparent] md:border-r-0 md:bottom-1/2 md:right-0 md:translate-x-full md:translate-y-1/2 md:top-auto"></div>
+
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LoginInput;
--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,37 +1,88 @@
-import { Form, Formik } from "formik";
+import {
+  ErrorMessage,
+  Form,
+  Formik,
+  FormikProps,
+  useFormik,
+  validateYupSchema,
+} from "formik";
+import * as Yup from "yup";
 import React from "react";
 import { Link } from "react-router-dom";
+import LoginInput from "../../components/inputs/login-input";
+
+interface LoginFormState {
+  email: string;
+  password: string;
+}
+
+const initialValues: LoginFormState = {
+  email: "",
+  password: "",
+};
+
+const onSubmit = (values: LoginFormState) => {};
+
+const validationSchema = Yup.object({
+  email: Yup.string().required("Email address is required!").email("Must be a valid email"),
+  password: Yup.string().required("Password is required!"),
+});
 
 const Login = () => {
   return (
     <div className="login">
       <div className="login_wrapper bg-bg__secondary">
-        <div className="login_wrap h-[78vh] text-color__primary">
-          <div className="login-1 w-80 mx-auto">
-            <img src="/icons/facebook.svg" alt="facebook logo" />
-            <span className="text-[1.25rem]">
+        <div className="login_wrap h-[78vh] text-color__primary md:flex md:items-center md:max-w-5xl md:mx-auto">
+          <div className="login-1 w-80 mx-auto md:flex md:flex-col md:w-1/2 md:mb-[15vh]">
+            <img className="md:w-[300px] md:-ml-[1.7rem]" src="/icons/facebook.svg" alt="facebook logo" />
+            <span className="md:text-[1.25rem] font-semibold md:textlg">
               Facebook helps you connect and share with the people in your life
             </span>
           </div>
           <div className="login-2 text-center">
             <div className="login-2_wrap flex flex-col items-center gap-4 bg-primary shadow-md p-4 pb-8 w-96 h-fit mx-auto rounded-lg my-4">
               <Formik
-              initialValues={{sl:""}}
-
-                onSubmit={()=>{}}
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+                enableReinitialize
               >
-                <Form>
-                  <input type="text" />
-                  <input type="text" />
-                  <button className="button_presets w-full h-12 text-base font-semibold select-none" type="submit">Log In</button>
-                </Form>
+                {(props:FormikProps<LoginFormState>) => (
+                  <Form>
+                    <LoginInput
+                      type="text"
+                      name="email"
+                      placeholder="Enter your address or phone number"
+                    />
+                    <LoginInput
+                      type="password"
+                      name="password"
+                      autoComplete=""
+                      placeholder="Enter your password"
+                      bottom={true}
+                    />
+                    <button
+                      className="button_presets w-full h-12 text-base font-semibold select-none"
+                      type="submit"
+                    >
+                      Log In
+                    </button>
+                  </Form>
+                )}
               </Formik>
-              <Link className="text-blue__color text-sm cursor-pointer hover:underline" to="/forgot">Forgot password?</Link>
+              <Link
+                className="text-blue__color text-sm cursor-pointer hover:underline"
+                to="/forgot"
+              >
+                Forgot password?
+              </Link>
               <div className="line_splitter divide-y-2 bg-bg__third w-full h-[0.2px]"></div>
-              <button className="button_presets bg-green__color w-3/4 font-semibold text-base mt-4">Create Account</button>
+              <button className="button_presets bg-green__color w-3/4 font-semibold text-base mt-4">
+                Create Account
+              </button>
             </div>
-            <Link className="text-base" to="">
-              <strong>Create a page</strong>
+            <Link className="text-base" to="/">
+              <strong>Create a page </strong>
               for a celebrity, brand or business
             </Link>
           </div>
