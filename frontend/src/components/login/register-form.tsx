0f8ea4a5fb1eb46f110a2174a6fb6f3627c29440
Vikas <vk.pratap98@gmail.com>
2022-09-02 10:17:35 +0530

    form gender value handler and bug is fixed

--- frontend/src/components/login/register-form.tsx
+++ frontend/src/components/login/register-form.tsx
@@ -10,7 +10,7 @@ interface RegisterFormState {
   bYear: number;
   bMonth: number;
   bDay: number;
-  gender: string;
+  gender: "male" | "female" | "custom" | null;
   daysListOfMonth: number[];
 }
 
@@ -22,7 +22,7 @@ const initialValues: RegisterFormState = {
   bYear: new Date().getFullYear(),
   bMonth: new Date().getMonth() + 1,
   bDay: new Date().getDate(),
-  gender: "",
+  gender: null,
   daysListOfMonth: [],
 };
 
@@ -47,7 +47,6 @@ const RegisterForm = () => {
     () => getDaysList(initialValues.bYear, initialValues.bMonth),
     []
   );
-  console.log(daysList, "daysLIst");
 
   return (
     <div className="custom-backdrop__blur ">
@@ -63,145 +62,162 @@ const RegisterForm = () => {
           initialValues={{ ...initialValues, daysListOfMonth: daysList }}
           onSubmit={() => {}}
         >
-          {(formik) => (
-            <Form className="register_form w-full flex flex-col items-center">
-              <div className="regi_line py-1.5 flex flex-col gap-1.5">
-                <RegisterInput
-                  type="text"
-                  placeholder="First name"
-                  name="first_name"
-                />
-                <RegisterInput
-                  type="text"
-                  placeholder="Last name"
-                  name="last_name"
-                />
-              </div>
-              <div className="regi_line py-2 pt-0 flex flex-col gap-1.5">
-                <RegisterInput
-                  type="text"
-                  placeholder="Mobile number or Email address"
-                  name="email"
-                />
-                <RegisterInput
-                  type="password"
-                  placeholder="New Password"
-                  name="password"
-                  autoComplete="on"
-                />
-              </div>
-              <div className="reg_col relative self-start mb-2 px-2">
-                <div className="reg_line_header flex items-center gap-1 text-sm text-color__secondary">
-                  Date of birth <i className="info_icon mt-0.5" />
+          {(formik) => {
+            return (
+              <Form className="register_form w-full flex flex-col items-center">
+                <div className="regi_line py-1.5 flex flex-col gap-1.5">
+                  <RegisterInput
+                    type="text"
+                    placeholder="First name"
+                    name="first_name"
+                  />
+                  <RegisterInput
+                    type="text"
+                    placeholder="Last name"
+                    name="last_name"
+                  />
                 </div>
-                <div className="reg_grid mt-1.5 grid grid-cols-3 gap-2.5 h-9 w-full">
-                  <select
-                    className="w-24 text-base text-color__primary bg-primary rounded-md cursor-pointer border border-dark_color__secondary outline-none"
-                    name="bDay"
-                    value={formik.values.bDay}
-                    onChange={formik.handleChange}
-                  >
-                    {formik.values.daysListOfMonth.map((date) => (
-                      <option key={date} value={formik.values.bDay}>
-                        {date}
-                      </option>
-                    ))}
-                  </select>
-                  <select
-                    className="w-24 text-base text-color__primary bg-primary rounded-md cursor-pointer border border-dark_color__secondary outline-none"
-                    name="bMonth"
-                    value={formik.values.bMonth}
-                    onChange={(e) => {
-                      const { name, value } = e.target;
-                      formik.setFieldValue(name, value);
-                      const _daysList = getDaysList(
-                        formik.values.bYear,
-                        Number(value)
-                      );
-                      formik.setFieldValue("daysListOfMonth", _daysList);
-                    }}
-                  >
-                    {months.map((month) => (
-                      <option key={month} value={month}>
-                        {month}
-                      </option>
-                    ))}
-                  </select>
-                  <select
-                    className="w-24 text-base text-color__primary bg-primary rounded-md cursor-pointer border border-dark_color__secondary outline-none"
-                    name="bYear"
-                    value={formik.values.bYear}
-                    onChange={(e) => {
-                      const { name, value } = e.target;
-                      formik.setFieldValue(name, value);
-                      const _daysList = getDaysList(
-                        Number(value),
-                        formik.values.bMonth
-                      );
-                      formik.setFieldValue("daysListOfMonth", _daysList);
-                    }}
-                  >
-                    {years.map((year) => (
-                      <option key={year} value={year}>
-                        {year}
-                      </option>
-                    ))}
-                  </select>
+                <div className="regi_line py-2 pt-0 flex flex-col gap-1.5">
+                  <RegisterInput
+                    type="text"
+                    placeholder="Mobile number or Email address"
+                    name="email"
+                  />
+                  <RegisterInput
+                    type="password"
+                    placeholder="New Password"
+                    name="password"
+                    autoComplete="on"
+                  />
                 </div>
-              </div>
-              <div className="reg_col relative self-start mb-2 px-2">
-                <div className="reg_line_header flex items-center gap-1 text-sm text-color__secondary">
-                  Gender <i className="info_icon mt-0.5" />
+                <div className="reg_col relative self-start mb-2 px-2">
+                  <div className="reg_line_header flex items-center gap-1 text-sm text-color__secondary">
+                    Date of birth <i className="info_icon mt-0.5" />
+                  </div>
+                  <div className="reg_grid mt-1.5 grid grid-cols-3 gap-2.5 h-9 w-full">
+                    <select
+                      className="w-24 text-base text-color__primary bg-primary rounded-md cursor-pointer border border-dark_color__secondary outline-none"
+                      name="bDay"
+                      value={formik.values.bDay}
+                      onChange={formik.handleChange}
+                    >
+                      {formik.values.daysListOfMonth.map((date) => (
+                        <option key={date} value={date}>
+                          {date}
+                        </option>
+                      ))}
+                    </select>
+                    <select
+                      className="w-24 text-base text-color__primary bg-primary rounded-md cursor-pointer border border-dark_color__secondary outline-none"
+                      name="bMonth"
+                      value={formik.values.bMonth}
+                      onChange={(e) => {
+                        const { name, value } = e.target;
+                        formik.setFieldValue(name, value);
+                        const _daysList = getDaysList(
+                          formik.values.bYear,
+                          Number(value)
+                        );
+                        formik.setFieldValue("daysListOfMonth", _daysList);
+                      }}
+                    >
+                      {months.map((month) => (
+                        <option key={month} value={month}>
+                          {month}
+                        </option>
+                      ))}
+                    </select>
+                    <select
+                      className="w-24 text-base text-color__primary bg-primary rounded-md cursor-pointer border border-dark_color__secondary outline-none"
+                      name="bYear"
+                      value={formik.values.bYear}
+                      onChange={(e) => {
+                        const { name, value } = e.target;
+                        formik.setFieldValue(name, value);
+                        const _daysList = getDaysList(
+                          Number(value),
+                          formik.values.bMonth
+                        );
+                        formik.setFieldValue("daysListOfMonth", _daysList);
+                      }}
+                    >
+                      {years.map((year) => (
+                        <option key={year} value={year}>
+                          {year}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
                 </div>
-                <div className="reg_grid mt-1.5 grid grid-cols-3 gap-2.5 h-9 w-full">
-                  <label
-                    className="w-24 flex items-center justify-between text-base text-color__primary rounded-md cursor-pointer border border-dark_color__secondary px-2.5"
-                    htmlFor="male"
-                  >
-                    Male
-                    <input type="radio" value="male" name="gender" id="male" />
-                  </label>
-                  <label
-                    className="w-24 flex items-center justify-between text-base text-color__primary rounded-md cursor-pointer border border-dark_color__secondary px-2.5"
-                    htmlFor="female"
-                  >
-                    Female
-                    <input
-                      type="radio"
-                      value="female"
-                      name="gender"
-                      id="female"
-                    />
-                  </label>
-                  <label
-                    className="w-24 flex items-center justify-between text-base text-color__primary rounded-md cursor-pointer border border-dark_color__secondary px-2.5"
-                    htmlFor="custom"
-                  >
-                    Custom
-                    <input
-                      type="radio"
-                      value="custom"
-                      name="gender"
-                      id="custom"
-                    />
-                  </label>
+                <div className="reg_col relative self-start mb-2 px-2">
+                  <div className="reg_line_header flex items-center gap-1 text-sm text-color__secondary">
+                    Gender <i className="info_icon mt-0.5" />
+                  </div>
+                  <div className="reg_grid mt-1.5 grid grid-cols-3 gap-2.5 h-9 w-full">
+                    <label
+                      className="w-24 flex items-center justify-between text-base text-color__primary rounded-md cursor-pointer border border-dark_color__secondary px-2.5"
+                      htmlFor="male"
+                    >
+                      Male
+                      <input
+                        type="radio"
+                        value="male"
+                        name="gender"
+                        id="male"
+                        checked={formik.values.gender === "male"}
+                        onChange={formik.handleChange}
+                      />
+                    </label>
+                    <label
+                      className="w-24 flex items-center justify-between text-base text-color__primary rounded-md cursor-pointer border border-dark_color__secondary px-2.5"
+                      htmlFor="female"
+                    >
+                      Female
+                      <input
+                        type="radio"
+                        value="female"
+                        name="gender"
+                        id="female"
+                        checked={formik.values.gender === "female"}
+                        onChange={formik.handleChange}
+                      />
+                    </label>
+                    <label
+                      className="w-24 flex items-center justify-between text-base text-color__primary rounded-md cursor-pointer border border-dark_color__secondary px-2.5"
+                      htmlFor="custom"
+                    >
+                      Custom
+                      <input
+                        type="radio"
+                        value="custom"
+                        name="gender"
+                        id="custom"
+                        checked={formik.values.gender === "custom"}
+                        onChange={formik.handleChange}
+                      />
+                    </label>
+                  </div>
                 </div>
-              </div>
-              <div className="reg_infos text-xs mt-2.5 text-color__secondary">
-                By clicking Sign Up, you agree to our{" "}
-                <span className="text-blue__color">
-                  Terms, Data Policy &npsp;
-                </span>{" "}
-                and <span className="text-blue__color">Cookie Policy</span> You
-                may receive SMS notifications from us and can opt at any time.
-              </div>
-              <div className="reg_btn_wrapper w-full flex items-center justify-center mb-2.5 mt-5 ">
-                <button className="button_presets w-3/4 bg-green__color open_signup">
-                  Sign Up
-                </button>
-              </div>
-            </Form>
-          )}
+                <div className="reg_infos text-xs mt-2.5 text-color__secondary">
+                  By clicking Sign Up, you agree to our{" "}
+                  <span className="text-blue__color">
+                    Terms, Data Policy &npsp;
+                  </span>{" "}
+                  and <span className="text-blue__color">Cookie Policy</span>{" "}
+                  You may receive SMS notifications from us and can opt at any
+                  time.
+                </div>
+                <div className="reg_btn_wrapper w-full flex items-center justify-center mb-2.5 mt-5 ">
+                  <button
+                    type="submit"
+                    className="button_presets w-3/4 bg-green__color open_signup"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </Form>
+            );
+          }}
         </Formik>
       </div>
     </div>
